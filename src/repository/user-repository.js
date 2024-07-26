25af46ec4c676fa1feb02c80bb670828c74af23b
Shashant Shekhar <shashantshekhar10@gmail.com>
2024-07-27 04:12:48 +0530

    create some function for authentications

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,19 +1,19 @@
 import User from "../models/user.js";
 import CrudRepository from "./crud-repository.js";
-// import Like from "../models/like.js";
 
 class UserRepository extends CrudRepository {
   constructor() {
     super(User);
   }
 
-//   async findByUserAndLikeable(data) {
-//     try {
-//       const like = await Like.findOne(data);
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
+  async findBy(data) {
+    try {
+      const response = await User.findOne(data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default UserRepository;
